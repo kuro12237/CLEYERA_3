9cd3f00acf3bc5a601a8e149d8fb2f12f8790eb6
kuro12237 <115063113+kuro12237@users.noreply.github.com>
2023-07-17 14:18:46 +0900

    stetePattern完成

--- main.cpp
+++ main.cpp
@@ -1,122 +1,17 @@
 #include"CLEYERA/CLEYERA.h"
+#include"Game/Game.h"
 
 #define TriangleMax 3
 
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
+	Game::Initialize();
 
-	MatrixTransform* matrixTransform = new MatrixTransform();
+	Game::Run();
 
-	const int32_t kClientWidth = 1280;
-	const int32_t kClientHeight = 720;
-
-	Cleyera::Initialize(kClientWidth, kClientHeight);
-
-#pragma region ‰Šú‰»
-
-		Triangle triangle[TriangleMax];
-		//t,l,r
-		triangle[0].position =
-		{ {0.0f,0.0,0.0f},{-0.5f,-1.0f,0.0f},{0.5f,-1.0f,0.0f } };
-		triangle[0].Resources = Cleyera::CreateShapeResource();
-		triangle[0].Color = BLACK;
-		triangle[0].matrix = matrixTransform->Identity();
-
-		triangle[1].position =
-		{
-		{ 0.0f, 1.0f, 0.0f},
-		{ -0.5f,0.0f,0.0f },
-		{ 0.5f,-0.0f,0.0f} };
-		triangle[1].Resources = Cleyera::CreateSpriteResource();
-		triangle[1].Color = WHITE;
-		triangle[1].matrix = matrixTransform->Identity();
+	Game::Finalize();
 
 
-
-		texResourceProperty tex;
-
-		tex = Cleyera::LoadTex("CLEYERA/DefaultResources/uvChecker.png");
-
-	
-		triangle[2].position =
-		{
-		{ -0.5f, 1.0f, 0.0f},
-		{ -1.0f,0.0f,0.0f },
-		{ 0.0f,0.0f,0.0f} };
-		triangle[2].Resources = Cleyera::CreateSpriteResource();
-		triangle[2].Color = WHITE;
-		triangle[2].matrix = matrixTransform->Identity();
-#pragma endregion 
-
-	MSG msg{};
-
-	BYTE key[256] = {0};
-
-	while (msg.message!=WM_QUIT)
-	{
-		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
-		{
-			Cleyera::WinMSG(msg);
-		}
-
-		Cleyera::BeginFlame(kClientWidth, kClientHeight);
-
-		Input::BeginFlame(key);
-
-#pragma region XVˆ—
-
-		if (key[DIK_0])
-		{
-			OutputDebugStringA("Hit\n");
-			Cleyera::TriangleDraw(triangle[0].position,
-				triangle[0].Color, triangle[0].matrix,
-				triangle[0].Resources);
-
-		}
-	
-	
-#pragma endregion
-		
-
-
-
-#pragma region •`‰æˆ—
-	
-		
-
-			
-	     
-		Cleyera::SpriteTriangleDraw(
-			triangle[1].position,
-			triangle[1].Color,
-			triangle[1].matrix,
-			triangle[1].Resources,
-			tex);
-		
-		Cleyera::SpriteTriangleDraw(
-			triangle[2].position,
-			triangle[2].Color,
-			triangle[2].matrix,
-			triangle[2].Resources,
-			tex);
-
-#pragma endregion
-
-
-		Cleyera::EndFlame();
-	}
-
-#pragma region ’¸“_‚È‚Ç‚Ì‰ğ•ú
-
-	Cleyera::TriangleResourceRelease(triangle[0].Resources);
-	
-	Cleyera::SpriteTriangleResourceRelease(triangle[1].Resources, tex);
-	Cleyera::SpriteTriangleResourceRelease(triangle[2].Resources, tex);
-
-#pragma endregion
-
-	
-	Cleyera::Finalize();
 	return 0;
 }
